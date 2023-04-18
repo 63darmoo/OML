28bd9b95b1e602a51127b6a395535af585103446
63darmoo <129257437+63darmoo@users.noreply.github.com>
2023-04-17 19:05:51 -0700

    js

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 let hasJob = false;
 if (hasJob){
   // I have a Job
-  document.writeln("Thanks for visiting, I'm currently employed");
+  showMessage("Thanks for visiting, I'm currently employed");
 }
 else {
   // I need a Job
-  document.writeln("Please look around, I'm currently looking for a position.");
+  showMessage("Please look around, I'm currently looking for a position.");
 }
 
-let today = new Date();
-let dayOfweek = today.get
+let today = new Date("08/01/2021");
+let dayOfweek = today.getDay();
+if (dayOfweek === 0 || dayOfweek === 6) {
+      showMessage("Since it is the weekend, please be patient with my returning your email.");
+}
+
+function showMessage (message) {
+  document.writeln("<p>" + message + "</p>")
+  document.writeln("<hr/>");
+}
